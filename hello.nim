20f7aabecd7e57d96d0d1ba0fe8bf79c88d377dc
佐伯紘二 <saikou9901@gmail.com>
2018-01-22 22:45:47 +0900

    echoをいろいろテスト

--- hello.nim
+++ hello.nim
@@ -1,7 +1,48 @@
+#[
+  これはブロックコメント。
+  Visual Studio CodeのNim Extensionをインストールすると、ブロックコメント閉じも自動生成。
+  改行でインデントもしてくれる。
+  https://marketplace.visualstudio.com/items?itemName=kosz78.nim
+]#
+
 # this is a comment
 echo "Whats your name? :"
 var name: string = readLine(stdin)
 echo "Hi, ", name, "!"
 
-echo "not raw text \""
-echo r"raw text \" ## document comment
+echo "not raw text \"" ## document comment
+
+echo """ここは
+discardblock"""
+
+echo r"raw text \"
+
+echo """
+ここから打ち始めても、
+さっきの「ここは」があったところに余計な改行が入ったりしない。"""
+
+echo "Hiからここまで空行ないけど"
+
+echo """
+閉じる「\"\"\"」を改行後にすると
+"""
+
+echo "この上は空行になる"
+
+#[
+  実行結果はこちら
+  -----------------------------------------------
+  Whats your name? :
+  koji
+  Hi, koji!
+  not raw text "
+  ここは
+  discardblock
+  raw text \
+  ここから打ち始めても、
+  さっきの「ここは」があったところに余計な改行が入ったりしない。
+  Hiからここまで空行ないけど
+  閉じる「\"\"\"」を改行後にすると
+
+  この上は空行になる
+]#
